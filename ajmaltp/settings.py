7869256ef3e88edfee53b5a9d0add64b85e3f05d
asir <asirm0658@gmail.com>
2023-05-08 10:08:10 +0530

    sec commit

--- ajmaltp/settings.py
+++ ajmaltp/settings.py
@@ -74,17 +74,7 @@ DATABASES = {
         "PORT": "",
     }
 }
-# Define DATABASES (postgres-production)
-# DATABASES = {
-#     "default": {
-#         "ENGINE": config("DB_ENGINE", default="django.db.backends.sqlite3"),
-#         "NAME": config("DB_NAME", default=BASE_DIR / "db.sqlite3"),
-#         "USER": config("DB_USER", default=""),
-#         "PASSWORD": config("DB_PASSWORD", default=""),
-#         "HOST": config("DB_HOST", default="localhost"),
-#         "PORT": "",
-#     }
-# }
+
 
 
 # Password validation
